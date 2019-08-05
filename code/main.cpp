50e6724366727d289d0decc2412402a55f95112b
Joao Costa <jmpc1111@gmail.com>
2019-08-05 10:24:07 +0100

    Basic structure - step 2.

---
+++ code/main.cpp
@@ -0,0 +1,36 @@
+// *****************************************************************************************
+//
+// File description:
+//
+// Author:	Joao Costa
+// Purpose:	Monkey main C++ function
+//			Read the README.md file for more information on the project
+//
+// *****************************************************************************************
+
+
+// *****************************************************************************************
+//
+// Section: Import headers
+//
+// *****************************************************************************************
+
+// Include OSAPI C++ headers
+
+// Include Standard headers
+#include <cstdlib>
+
+// Import module declarations
+#include "monkey_defs.hh"
+
+// *****************************************************************************************
+//
+// Section: Function definition
+//
+// *****************************************************************************************
+
+int MONKEY_MAIN( int argc, t_char * argv[] )
+{
+
+ return EXIT_SUCCESS;
+}
---
+++ code/monkey_defs.hh
@@ -0,0 +1,41 @@
+// *****************************************************************************************
+//
+// File description:
+//
+// Author:	Joao Costa
+// Purpose:	Provide the definitions of the Monkey project
+//
+// *****************************************************************************************
+
+#ifndef MONKEY_DEFS_HH_
+#define MONKEY_DEFS_HH_
+
+// *****************************************************************************************
+//
+// Section: Import headers
+//
+// *****************************************************************************************
+
+// Import C++ system headers
+
+// *****************************************************************************************
+//
+// Section: Type declaration/definition
+//
+// *****************************************************************************************
+
+#if defined(OS_LINUX) || defined(__linux__) || defined(LINUX)
+
+ #define OS_LINUX		1
+ #define MONKEY_MAIN	main
+ #define t_char			char
+
+#elif defined(OS_WINDOWS) || defined(_WIN32) || defined(_WIN64)
+
+ #define OS_WINDOWS		1
+ #define MONKEY_MAIN	wmain
+ #define t_char			wchar_t
+
+#endif
+
+#endif // MONKEY_DEFS_HH_
